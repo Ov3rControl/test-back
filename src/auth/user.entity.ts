cc995288ef4fcffdd98b192b469787fb6b10e718
Mohamed <ov3rcontrol@hotmail.com>
2020-12-04 15:48:51 +0200

    :sparkles: Add UserEntity

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,29 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+
+@Entity()
+@Unique(['username'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', onUpdate: 'CURRENT_TIMESTAMP', nullable: true })
+  updatedAt: Date;
+}
