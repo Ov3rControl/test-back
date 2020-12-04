2e89431720cc5e0491a0166698253ee8da30b7d4
Mohamed <ov3rcontrol@hotmail.com>
2020-12-04 15:49:52 +0200

    :sparkles: Add Signup Service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { LoginCred, LoginRes } from './auth.model';
 import { AuthService } from './auth.service';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body(ValidationPipe) authCredentialsDTO: AuthCredentialsDTO) {
+    return this.authService.signUp(authCredentialsDTO);
+  }
 
   @Post()
   login(@Body() user: LoginCred): LoginRes {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { LoginCred, LoginRes } from './auth.model';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDTO);
+  }
+
   login(body: LoginCred): LoginRes {
     if (body.username === 'admin' && body.password === 'admin')
       return { role: 'admin', auth: true, token: 'UglyStaticAdminToken' };
